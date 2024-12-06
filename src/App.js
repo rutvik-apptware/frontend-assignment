4bf3bbb1e96eb0c02f8abcf637714e2bdc76ded6
rutvikpumak <rutvikumak@gmail.com>
2024-12-06 20:21:05 +0530

    feat / integrate API and render data in table

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
+import React, { useState, useEffect } from "react";
+import Table from "./components/Table";
+import { Loader } from "./components/Loader";
 import "./App.css";
+const App = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-function App() {
-  return <div className="App">Initial Commit</div>;
-}
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://raw.githubusercontent.com/saaslabsco/frontend-assignment/refs/heads/master/frontend-assignment.json"
+        );
+        const result = await response.json();
+        setData(result || []);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="app">
+      <h1 className="title">Highly Rated Kickstarter Projects</h1>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Table
+            data={data.map((item, index) => ({
+              "S.No.": index + 1,
+              "Percentage Funded": item["percentage.funded"],
+              "Amount Pledged": item["amt.pledged"],
+            }))}
+          />
+        </>
+      )}
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/Table.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Table = ({ data }) => {
+  if (data.length === 0) {
+    return <p>No data available.</p>;
+  }
+
+  const headers = Object.keys(data[0]);
+
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          {headers.map((header) => (
+            <th key={header}>{header}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, index) => (
+          <tr key={index}>
+            {Object.values(row).map((value, i) => (
+              <td key={i}>{value}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
